102a4adfc70a646ba982adb6aae711b2393b60c9
Pedro Wiezel <131211509+pwiez@users.noreply.github.com>
2025-05-28 09:09:24 -0300

    Initial Commit

---
+++ CineFiles/CineFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFilesApp.swift
+//  CineFiles
+//
+//  Created by Pedro Wiezel on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CineFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
